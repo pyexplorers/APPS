9cf52a46b8149c2fdcd08cb30845733b2492cff3
pyexplorers <pyexplorers@gmail.com>
2019-05-10 17:45:59 +0530

    GIT API Code

---
+++ apis/app/web/apis.py
@@ -0,0 +1,131 @@
+'''
+    Import module to be used as part of apis requests
+'''
+from flask import Flask, request
+import json
+import web_utils,web_configs
+import pdb
+
+''' 
+    Create the Flask app.
+    Perform all CRUD operations.
+    Process GET,POST,PUT,DELETE events with authentication    
+'''
+users_api = Flask(__name__)
+
+class Users_Stats:
+    '''
+        A Class to store and return information about multiple users
+        Records are inserted and fetched from Json/DB.
+        Define separate method calls for each REST Type
+            GET -> 
+                def users_information
+                @users_api.route('/user_info')
+            POST -> 
+                def post_users_information
+                @users_api.route('/post_user_info')
+    '''
+    
+    @users_api.route('/user_info')
+    def users_information():
+        '''
+            Given a key get all information w.r.t to an User
+            Search the cached data from MongoDB and return all
+            records that match the user query.
+            ************************************************
+                For all REST GET calls , results will be loaded 
+                from cache since hitting DB for query
+                can be avoided and data  from cache can be faster.
+
+                Data will be loaded from DB -> Cache periodically
+            ************************************************
+            @return:
+                Success -> Valid records that match user's query
+                Exception -> {"data":"ERROR"}
+        '''
+        # Get all users whose attributes matches the argument specified by the User
+        get_users_information = web_utils.Tasks.get_users_information(request.args.items(),request.authorization)
+        return json.dumps({"data":get_users_information})
+
+    @users_api.route('/post_user_info',methods=['POST'])
+    def post_users_information():
+        '''
+            On a REST POST call -> Make a new entry into MongoDB
+            Given that User specifies required attributes and that the authorization information
+            is correct then make an entry into the database.
+            @return:
+                Success -> {"data":"True"}
+                Exception -> {"data":"ERROR"}
+        '''
+        # Create a new document in an existing collection in the database
+        return web_utils.Tasks.post_users_information(request.args.to_dict(),request.authorization,
+            mongo_collection=web_utils.mongo_set_database_collection)
+
+    @users_api.route('/update_user_info',methods=['POST'])
+    def update_users_information():
+        '''
+            On a REST POST call -> Make an update to an existing entry in MongoDB
+            Given that User specifies required attributes and that the authorization information
+            is correct then make an update in the database.
+
+            Given an user_name attribute and update information , modify all records
+            in the database wherever such an user would exist.
+
+            @return:
+                Success -> {"data":"True"}
+                Exception -> {"data":"ERROR"}
+        '''
+        # Update an existing collection in the database
+        return web_utils.Tasks.update_users_information(request.args.to_dict(),request.authorization,
+            mongo_collection=web_utils.mongo_set_database_collection)
+
+    @users_api.route('/delete_user_info',methods=['POST'])
+    def delete_users_information():
+        '''
+            On a REST POST call -> Make an update to an existing entry in MongoDB
+            Given that User specifies required attributes and that the authorization information
+            is correct then make an update in the database.
+
+            Given an user_name attribute and update information , modify all records
+            in the database wherever such an user would exist.
+
+            @return:
+                Success -> {"data":"True"}
+                Exception -> {"data":"ERROR"}
+        '''
+        # Update an existing collection in the database
+        return web_utils.Tasks.delete_users_information(request.args.to_dict(),request.authorization,
+            mongo_collection=web_utils.mongo_set_database_collection)
+
+'''
+    Run the pre-requisite step for processing
+'''
+def main():
+    '''
+        This function is to be run before the server is booted
+    '''
+    # Read the json file and load the records into memory
+    web_utils.Tasks.set_process_read_frequent()
+    # Mongo Collection for all mongo tasks
+    web_utils.mongo_set_database_collection = web_utils.Tasks.set_database()
+
+'''
+    Start the Flask Server and do all REST Operations
+'''        
+if __name__ == "__main__":
+    # Invoke the pre-requisite step
+    main()
+    # Run the app in port 4000 and run in debug mode
+    users_api.run(debug=True,port=4000)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ apis/app/web/web_utils.py
@@ -0,0 +1,185 @@
+'''
+    Perform all activities related to the REST API Queries
+    Connect with Database and do insert,fetch,update and delete operations
+'''
+import web_configs
+from core.utilities.pyutils import Py_Utils
+from core.metas.metas import authorize
+from core.databases.mongoapp.query import Mongo_Utils
+
+class Tasks:
+
+    @staticmethod
+    def set_database():
+        '''
+            Raise a connection with MongoDB
+            Select database and collection
+            Return the collections object
+            Do Insert, Fetch, Update and Delete
+        '''
+        # Connect to the database
+        connect_database = Mongo_Utils.get_connection(server_name=web_configs.MONGO_SERVER_NAME, server_port=web_configs.MONGO_SERVER_PORT)
+        # Fetch the databse 
+        fetch_database = Mongo_Utils.set_mongo_database(connection_object=connect_database, database_name=web_configs.MONGO_SERVER_DATABASE_NAME)
+        # Return the Collection object
+        return fetch_database.users_metadata
+
+    @staticmethod
+    def set_process_read_frequent():
+        '''
+            Set an infinite process to read the json files periodically and update the records
+            ##TODO## -> This Function object will be triggered periodically and contents be updated
+                from MongoDB
+        '''
+        #Class Variables to fetch & store the records -> File will be read once during module import
+        Tasks.DATASTORE_JSON_RECORDS = Py_Utils.read_json(web_configs.DATASTORE_JSON_FILE_CACHES)['records']
+        # Get all Usernames and store in a mapper
+        Tasks.DATASTORE_JSON_RECORDS_USER_NAME_ARRAY  = [str(json_records['user_name']).lower() for json_records in Tasks.DATASTORE_JSON_RECORDS]
+        return Tasks.DATASTORE_JSON_RECORDS
+    
+    @staticmethod
+    @authorize(credentials=web_configs.AUTHORIZATION_INFORMATION,invalid_credentials_error=web_configs.INVALID_CREDENTIALS_ERROR)
+    def get_users_information(mapper,authorize_info):
+        '''
+            Given a key-value pair, check the database for given combination and retreive required values.
+            *** The function will be executed only if the User issues the REST calls with proper credentials. ***
+            ***
+                For all GET Operations instead of querying the Database, we shall take the
+                records from the cache and return the filtered output. This cache will be
+                updated periodically.
+            ***
+            :param mapper: User requested keys & values
+            :param authorize_info: Auth Info required for processing REST Calls
+            @return:
+                Success -> All matching records that fit user's requests
+                Exception -> Empty Array
+        '''
+        try:
+            # Filter keys that are actually part of the system
+            keys_api_users_attributes = [each_user_key for each_user_key in mapper if str(each_user_key[0]).lstrip().rstrip() in web_configs.API_USERS_ATTRIBUTES]
+            # Do a Search on the existing datastore to filter records that fit the user's requests
+            return [each_record for each_record in Tasks.DATASTORE_JSON_RECORDS for each_user_key in keys_api_users_attributes \
+                if str(each_record[each_user_key[0]]).lower().__contains__(str(each_user_key[1]).lower())]
+        except Exception as error:
+            return ['ERROR']
+
+    @staticmethod
+    @authorize(credentials=web_configs.AUTHORIZATION_INFORMATION,invalid_credentials_error=web_configs.INVALID_CREDENTIALS_ERROR)
+    def post_users_information(mapper,authorize_info,mongo_collection=None):
+        '''
+            Given user management data from user, make an entry into database and return the confirmation.
+            *** The function will be executed only if the User issues the REST calls with proper credentials. ***
+            ***
+                For all POST Operations ,check the keys and values sent by the User and make an entry into the database.
+                Invalid keys will be filtered and only valid keys will be processed.
+                All keys required as a mandate shall be supplied by the User else user management DB will not
+                be updated with user's requests.
+                Invoke the supplied database and collection and insert the records.
+            ***
+            :param mapper: User recommended keys & values -> Dictionary format
+            :param authorize_info: Auth Info required for processing REST Calls
+            :param mongo_collection: Collection on which the records are to be inserted
+            @return:
+                Success -> Invoke database and add an entry
+                            Return True if records are inserted else False
+                Exception -> return ERROR
+        '''
+        try:
+            '''
+                Check if the User has posted all the required keys.
+                Insert into MongoDB only if all the requiredkeys are found.
+            '''
+            if bool(Py_Utils.validate_users_keys(mapper,web_configs.API_USERS_ATTRIBUTES,
+                    Tasks.DATASTORE_JSON_RECORDS_USER_NAME_ARRAY,check_user_exists=True)):
+                '''
+                    Do insert the records into the database -> This function will insert a new record(document) to the collection.
+                    Mongo_Utils.insert_records -> This function will handle both single and bulk inserts.   
+                '''
+                Mongo_Utils.insert_records(collection_object=mongo_collection, records=mapper)
+                # Return True -> Able to insert records into the database
+                return {"data":True}
+            # Return False -> Unable to insert records into the database
+            return {"data":False}
+        except Exception as error:
+            # Return error on exception
+            return {"data":"ERROR"}
+
+    @staticmethod
+    @authorize(credentials=web_configs.AUTHORIZATION_INFORMATION,invalid_credentials_error=web_configs.INVALID_CREDENTIALS_ERROR)
+    def update_users_information(mapper,authorize_info,mongo_collection=None):
+        '''
+            Given user management data from user, update all entry in the database and return the confirmation.
+            *** The function will be executed only if the User issues the REST calls with proper credentials. ***
+            ***
+                For all PUT Operations ,check the keys and values sent by the User and make an update in the database.
+                Invalid keys will be filtered and only valid keys will be processed.
+                All keys required as a mandate shall be supplied by the User else user management DB will not
+                be updated with user's requests.
+                Invoke the supplied database and collection and insert the records.
+            ***
+            :param mapper: User recommended keys & values -> Dictionary format
+            :param authorize_info: Auth Info required for processing REST Calls
+            :param mongo_collection: Collection on which the records are to be inserted
+            @return:
+                Success -> Invoke database and update all the documents in the collection.
+                            Return True if records are inserted else False
+                Exception -> return ERROR
+        '''
+        try:
+            '''
+                Check if the User has posted all the required keys.
+                Make an update in MongoDB only if all the required keys are found.
+            '''
+            if bool(Py_Utils.validate_users_keys(mapper,web_configs.API_USERS_ATTRIBUTES,Tasks.DATASTORE_JSON_RECORDS_USER_NAME_ARRAY)):
+                '''
+                    Do insert the records into the database -> This function will insert a new record(document) to the collection.
+                    Mongo_Utils.insert_records -> This function will handle both single and bulk inserts.   
+                '''
+                Mongo_Utils.update_records(collection_object=mongo_collection, records=mapper)
+                # Return True -> Able to insert records into the database
+                return {"data":True}
+            # Return False -> Unable to insert records into the database
+            return {"data":False}
+        except Exception as error:
+            # Return error on exception
+            return {"data":"ERROR"}
+
+    @staticmethod
+    @authorize(credentials=web_configs.AUTHORIZATION_INFORMATION,invalid_credentials_error=web_configs.INVALID_CREDENTIALS_ERROR)
+    def delete_users_information(mapper,authorize_info,mongo_collection=None):
+        '''
+            Given user management data from user, delete all entry in the database and return the confirmation.
+            *** The function will be executed only if the User issues the REST calls with proper credentials. ***
+            ***
+                For all DELETE Operations ,check the keys and values sent by the User and make a deletion in the database.
+                Invalid keys will be filtered and only valid keys will be processed.
+                All keys required as a mandate shall be supplied by the User else user management DB will not
+                be updated with user's requests.
+                Invoke the supplied database and collection and delete the documents.
+            ***
+            :param mapper: User recommended keys & values -> Dictionary format.
+            :param authorize_info: Auth Info required for processing REST Calls.
+            :param mongo_collection: Collection on which the documents are to be deleted.
+            @return:
+                Success -> Invoke database and delete all the documents in the collection.
+                            Return True if records are deleted else False.
+                Exception -> return ERROR
+        '''
+        try:
+            '''
+                Check if the User has posted all the required keys.
+                Make an deletion in MongoDB only if all the required keys are found.
+            '''
+            if bool(Py_Utils.validate_users_keys(mapper,web_configs.API_USERS_ATTRIBUTES,Tasks.DATASTORE_JSON_RECORDS_USER_NAME_ARRAY)):
+                '''
+                    Do insert the records into the database -> This function will insert a new record(document) to the collection.
+                    Mongo_Utils.insert_records -> This function will handle both single and bulk inserts.   
+                '''
+                Mongo_Utils.delete_records(collection_object=mongo_collection, records=mapper)
+                # Return True -> Able to insert records into the database
+                return {"data":True}
+            # Return False -> Unable to insert records into the database
+            return {"data":False}
+        except Exception as error:
+            # Return error on exception
+            return {"data":"ERROR"}
